bde0fe5bcb947322e6543e61674c34317c71b30f
nricaurt1 <nricaurt1@cuc.edu.co>
2024-09-16 09:41:08 -0500

    proyecto react 01

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import miLogo from './assets/img/logoReact.png'
+
+function App() {
+	return (
+		<>
+			<div>
+				<img src={miLogo} alt="💩" />
+				<h1 className="red">algo</h1>
+			</div>
+		</>
+	)
+}
+
+export default App
